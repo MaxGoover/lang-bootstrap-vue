dcda8fe5aceda5d86a5e83c30b9762fafe5946a3
maxgoover <maxgoover@gmail.com>
2020-07-22 01:33:36 +0300

    Fixed i18n errors

---
+++ src/i18n/messages/modules/ru.js
@@ -0,0 +1,54 @@
+export default {
+  app: {
+    name: 'Lang-bootstrap'
+  },
+  appHeader: {
+    grammar: 'Грамматика',
+    learnWords: 'Учить слова',
+    login: 'Войти',
+    logout: 'Выйти',
+    main: 'Главная',
+    myCabinet: 'Мой кабинет',
+    reading: 'Чтение',
+    signup: 'Зарегистрироваться',
+    video: 'Видео'
+  },
+  common: {
+    no: 'Нет',
+    yes: 'Да'
+  },
+  dimmer: {
+    askConfirmLogout: 'Вы действительно хотите выйти?'
+  },
+  error: {
+    minLengthError: 'Не менее {min} символов',
+    maxLengthError: 'Не более {max} символов',
+    pageNotFound: 'Страница не найдена',
+    tryFollowingPages: 'Попробуйте перейти на одну из следующих страниц:'
+  },
+  img: {
+    img1: '/img/kitty.png'
+  },
+  menu: {
+    back: 'Назад',
+    main: 'Главная'
+  },
+  pageBooks: {
+    goTo: 'Перейти',
+    level: 'Уровень',
+    load: 'Загрузить',
+    open: 'Открыть',
+    parts: 'частей',
+    search: 'Поиск'
+  },
+  pageSign: {
+    authorization: 'Авторизация',
+    password: 'Пароль',
+    passwordRequired: 'Необходимо ввести пароль',
+    registration: 'Регистрация',
+    rememberMe: 'Запомни меня',
+    signIn: 'Войти',
+    signUp: 'Зарегистрироваться',
+    username: 'Логин'
+  }
+}
