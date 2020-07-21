7caf18cb31ed243b5339d2ca0ec2683c7afc2371
maxgoover <maxgoover@gmail.com>
2020-07-22 00:39:39 +0300

    Installed bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
+import './registerServiceWorker'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './registerServiceWorker'
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
