54cb61416a4aa291d94938e9d5b5e6fd22bc73fb
maxgoover <maxgoover@gmail.com>
2020-07-23 01:41:50 +0300

    Fixed styles for appheader bootstrap

--- src/components/app/AppHeader.vue
+++ src/components/app/AppHeader.vue
@@ -1,102 +1,126 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">{{ $t('app.name') }}</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"/>
+  <nav class="navbar navbar-expand-md bg-light">
+    <!--Логотип-->
+    <a class="navbar-brand ml-2 ml-sm-3" href="#">
+      <img
+        alt="logo"
+        height="35"
+        src="../../../public/boot_32.png"
+        width="35"
+      >
+      {{ $t('app.name') }}
+    </a>
+
+    <!--Икнока меню-->
+    <button
+      class="navbar-toggler" type="button" data-toggle="collapse"
+      aria-controls="navbar"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+      data-target="#navbar"
+    >
+      <i class="fas fa-bars"/>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+    <!--Элементы меню-->
+    <div class="collapse navbar-collapse " id="navbar">
+      <ul class="navbar-nav">
+        <li class="nav-item ml-3 my-auto">
+          <a class="nav-link" href="#">Тренировки</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown
+        <li class="nav-item ml-3">
+          <a
+            class="nav-link disabled"
+            href="#"
+            tabindex="-1"
+            aria-disabled="true"
+          >
+            Disabled
           </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
         </li>
       </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
+
+      <!--Профиль пользователя-->
+      <div class="nav-item dropdown ml-auto">
+        <a
+          class="nav-link dropdown-toggle" id="navbarDropdown"
+          role="button" data-toggle="dropdown" aria-haspopup="true"
+          aria-expanded="false"
+        >
+          Имя пользователя
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </div>
     </div>
   </nav>
 </template>
 
 <script>
-  import {mapState} from 'vuex'
-  // import DialogWindow from '../../components/menu/DialogWindow'
-  // import MenuItem from '../../entities/MenuItem'
+    import {mapState} from 'vuex'
+    // import DialogWindow from '../../components/menu/DialogWindow'
+    // import MenuItem from '../../entities/MenuItem'
 
-  export default {
-    name: 'AppHeader',
-    components: {
-      // DialogWindow
-    },
-    data() {
-      return {toggleDrawer: false}
-    },
-    computed: {
-      ...mapState('authorization', ['isAuthorized']),
-      ...mapState('general', ['loading']),
-      menuItems() {
-        return this.isAuthorized ? [
-          // new MenuItem(
-          //   'mdi-eye',
-          //   this.$i18n.t('appHeader.reading'),
-          //   { name: 'Reading' }
-          // ),
-          // new MenuItem(
-          //   'mdi-eye',
-          //   this.$i18n.t('appHeader.video'),
-          //   { name: 'Video' }
-          // ),
-          // new MenuItem(
-          //   'mdi-account-edit',
-          //   this.$i18n.t('appHeader.grammar'),
-          //   { name: 'Grammar' }
-          // ),
-          // new MenuItem(
-          //   'mdi-logout',
-          //   this.$i18n.t('appHeader.logout'),
-          //   null,
-          //   this.showDialog
-          // )
-        ] : [
-          // new MenuItem(
-          //   'mdi-login',
-          //   this.$i18n.t('appHeader.login'),
-          //   { name: 'Login' }
-          // ),
-          // new MenuItem(
-          //   'mdi-lock-open',
-          //   this.$i18n.t('appHeader.signup'),
-          //   { name: 'Signup' }
-          // )
-        ]
-      }
-    },
-    methods: {
-      showDialog() {
-        this.$store.commit('menu/setShowDialog', true)
-      }
+    export default {
+        name: 'AppHeader',
+        components: {
+            // DialogWindow
+        },
+        data() {
+            return {toggleDrawer: false}
+        },
+        computed: {
+            ...mapState('authorization', ['isAuthorized']),
+            ...mapState('general', ['loading']),
+            menuItems() {
+                return this.isAuthorized ? [
+                    // new MenuItem(
+                    //   'mdi-eye',
+                    //   this.$i18n.t('appHeader.reading'),
+                    //   { name: 'Reading' }
+                    // ),
+                    // new MenuItem(
+                    //   'mdi-eye',
+                    //   this.$i18n.t('appHeader.video'),
+                    //   { name: 'Video' }
+                    // ),
+                    // new MenuItem(
+                    //   'mdi-account-edit',
+                    //   this.$i18n.t('appHeader.grammar'),
+                    //   { name: 'Grammar' }
+                    // ),
+                    // new MenuItem(
+                    //   'mdi-logout',
+                    //   this.$i18n.t('appHeader.logout'),
+                    //   null,
+                    //   this.showDialog
+                    // )
+                ] : [
+                    // new MenuItem(
+                    //   'mdi-login',
+                    //   this.$i18n.t('appHeader.login'),
+                    //   { name: 'Login' }
+                    // ),
+                    // new MenuItem(
+                    //   'mdi-lock-open',
+                    //   this.$i18n.t('appHeader.signup'),
+                    //   { name: 'Signup' }
+                    // )
+                ]
+            }
+        },
+        methods: {
+            showDialog() {
+                this.$store.commit('menu/setShowDialog', true)
+            }
+        }
     }
-  }
 </script>
 
 <style scoped>
-@import '../../assets/components/app/AppHeader.css';
+  @import '../../assets/components/app/AppHeader.css';
 </style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,20 +1,23 @@
 <template>
-  <b-container>
-    <b-row>
-      <div class="col-lg-2 d-none d-lg-block">
+  <div class="container">
+    <div class="row">
+      <div class=" d-none d-lg-block col-lg-2">
         on lg and wider screens
       </div>
-      <b-col md="12" lg="10">
-        10
-      </b-col>
-    </b-row>
-  </b-container>
+      <div class="col-md-12 col-lg-10">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+        architecto consequatur debitis dicta dolorem dolorum ea earum eum,
+        expedita, hic ipsum magnam molestiae necessitatibus officiis
+        perspiciatis quae, ratione sed voluptatum.
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'Main'
-}
+  export default {
+    name: 'Main'
+  }
 </script>
 
 <style scoped>
