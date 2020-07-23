a411f0ebc6d2b3a9a354f5334a53fce8e9688496
maxgoover <maxgoover@gmail.com>
2020-07-23 17:02:40 +0300

    Sticky navbar and footer

--- src/components/app/AppFooter.vue
+++ src/components/app/AppFooter.vue
@@ -1,19 +1,25 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <ul class="nav">
-          <li class="nav-item">
-            <span class="nav-link">&copy; 2022 Theme by psdfreebis.com</span>
-          </li>
-          <li class="nav-item"><a href="#" class="nav-link">Privacy</a></li>
-          <li class="nav-item"><a href="#" class="nav-link">Terms of Use</a>
-          </li>
-          <li class="nav-item"><a href="#" class="nav-link">Site Map</a></li>
-        </ul>
+  <footer>
+    <div class="copyright">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <ul class="nav">
+              <li class="nav-item">
+                <span class="nav-link">&copy; 2022 Theme by psdfreebis.com</span>
+              </li>
+              <li class="nav-item"><a href="#" class="nav-link">Privacy</a></li>
+              <li class="nav-item"><a href="#" class="nav-link">Terms of Use</a>
+              </li>
+              <li class="nav-item"><a href="#" class="nav-link">Site Map</a></li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
+    <div class="void"></div>
+  </footer>
+
 <!--  <v-footer app fixed>-->
 <!--    &copy; 2020-->
 <!--    <v-spacer/>Written with-->
@@ -28,5 +34,13 @@ export default {
 </script>
 
 <style scoped>
-
+.copyright {
+  background-color: #fff;
+  bottom: 0;
+  position: fixed;
+  width: 100%;
+}
+.void {
+  height: 40px;
+}
 </style>
--- src/components/app/AppHeader.vue
+++ src/components/app/AppHeader.vue
@@ -1,63 +1,66 @@
 <template>
-  <nav class="navbar navbar-expand-md bg-light">
-    <!--Логотип-->
-    <a class="navbar-brand ml-2 ml-sm-3" href="#">
-      <img
-        alt="logo"
-        height="35"
-        src="../../../public/boot_32.png"
-        width="35"
-      >
-      {{ $t('app.name') }}
-    </a>
-
-    <!--Икнока меню-->
-    <button
-      class="navbar-toggler" type="button" data-toggle="collapse"
-      aria-controls="navbar"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-      data-target="#navbar"
-    >
-      <i class="fas fa-bars"/>
-    </button>
-
-    <!--Элементы меню-->
-    <div class="collapse navbar-collapse " id="navbar">
-      <ul class="navbar-nav">
-        <li class="nav-item ml-3 my-auto">
-          <a class="nav-link" href="#">Тренировки</a>
-        </li>
-        <li class="nav-item ml-3">
-          <a
-            class="nav-link disabled"
-            href="#"
-            tabindex="-1"
-            aria-disabled="true"
-          >
-            Disabled
-          </a>
-        </li>
-      </ul>
-
-      <!--Профиль пользователя-->
-      <div class="nav-item dropdown ml-auto">
-        <a
-          class="nav-link dropdown-toggle" id="navbarDropdown"
-          role="button" data-toggle="dropdown" aria-haspopup="true"
-          aria-expanded="false"
+  <header>
+    <nav class="navbar navbar-expand-md bg-light">
+      <!--Логотип-->
+      <a class="navbar-brand ml-2 ml-sm-3" href="#">
+        <img
+          alt="logo"
+          height="35"
+          src="../../../public/boot_32.png"
+          width="35"
         >
-          Имя пользователя
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+        {{ $t('app.name') }}
+      </a>
+
+      <!--Икнока меню-->
+      <button
+        class="navbar-toggler" type="button" data-toggle="collapse"
+        aria-controls="navbar"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+        data-target="#navbar"
+      >
+        <i class="fas fa-bars"/>
+      </button>
+
+      <!--Элементы меню-->
+      <div class="collapse navbar-collapse" id="navbar">
+        <ul class="navbar-nav">
+          <li class="nav-item ml-3 my-auto">
+            <a class="nav-link" href="#">Тренировки</a>
+          </li>
+          <li class="nav-item ml-3">
+            <a
+              class="nav-link disabled"
+              href="#"
+              tabindex="-1"
+              aria-disabled="true"
+            >
+              Disabled
+            </a>
+          </li>
+        </ul>
+
+        <!--Профиль пользователя-->
+        <div class="nav-item dropdown ml-auto">
+          <a
+            class="nav-link dropdown-toggle" id="navbarDropdown"
+            role="button" data-toggle="dropdown" aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Имя пользователя
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
         </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+    <div class="void"></div>
+  </header>
 </template>
 
 <script>
@@ -123,4 +126,7 @@
 
 <style scoped>
   @import '../../assets/components/app/AppHeader.css';
+  .void {
+    height: 61px;
+  }
 </style>
